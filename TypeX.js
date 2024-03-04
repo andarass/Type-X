7af4e4445fa9da111132bc7c4714acc0ec53f8bd
Andara Puteri Syalsabella <91397243+andarass@users.noreply.github.com>
2024-03-05 06:59:19 +0700

    Update File TypeX

--- TypeX.js
+++ TypeX.js
@@ -1,7 +1,40 @@
+const WORDS = [ "game", "day", "java", "script", "rainbow", "program", "p5", "bunny", "youtube", "github" ];
+
+var focus; // Asteroid the player is currently typing out
+var field = [];
+
+var score = 0;
+
+var planetCrust; // color of ship
+var planetMantle; // color of mantle
+
+var ship; // color of ship
+
 function setup() {
     createCanvas(500, 500)
+
+    planetCrust = randomColor();
+    planetMantle = randomColor();
+    ship = randomColor();
+
+    field.push(new Asteroid(random(width - 150) + 75, 0, random(WORDS), randomColor()));
+
+    focus = null;
 }
 
 function draw () {
+
     background(51);
+
+    drawBase();
+    drawLazer();
+    drawScore();
+
+    handleField();
+}
+
+function keyPressed() {
+
+    console.log(keyCode);
+
 }
