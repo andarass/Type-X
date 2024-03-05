afe90cbe8023bd65cc999177ba225ed0339cb6da
Andara Puteri Syalsabella <91397243+andarass@users.noreply.github.com>
2024-03-05 07:46:25 +0700

    Update File TypeX

--- TypeX.js
+++ TypeX.js
@@ -33,8 +33,56 @@ function draw () {
     handleField();
 }
 
+/**
+ * updates & draws Asteroids
+ * manages field array
+ * increments score
+ * manages focus
+ * creates Asteroids
+ */
+function handleField() {
+    for (var i = field.length - 1; i >= 0; i--) {
+        field[i].update();
+
+        if(field[i].intact) {
+            // asteroid is still on-screen
+
+            field[i].draw();
+        } else {
+            // asteroid has been destroyed
+
+            score += field[i].text.length;
+            field.splice(i, 1); // delete from array
+            focus = null;
+        }
+    }
+
+    /* attempt new Asteroid */
+    if (frameCount % 60 === 0) { // every second
+
+        if (random() > map(score, 0, 1000, 0.8, 0.01)) { // more difficult as game progresses
+
+            field.push(new Asteroid(random(width - 150) + 75, 0, random(WORDS), randomColor()));
+        }
+    }
+}
+
+/**
+ * handles user input
+ */
 function keyPressed() {
 
-    console.log(keyCode);
+    if (focus) {
+        // if we have honed in on a specific Asteroid
 
+        focus.erode(keyCode);
+    } else {
+        // find the asteroid to target
+
+        focus = findAsteroid(keyCode, field);
+
+        if (focus) {
+            focus.erode(keyCode);
+        }
+    }
 }
