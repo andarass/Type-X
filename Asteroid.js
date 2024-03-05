ed28493f9d42613ba4fac81654cb9c52537a7d7f
Andara Puteri Syalsabella <91397243+andarass@users.noreply.github.com>
2024-03-05 07:19:23 +0700

    Update File Asteroid

--- Asteroid.js
+++ Asteroid.js
@@ -1,8 +1,8 @@
-function Asteroid(x, y, text, c) {
+function Asteroid(x, y, text, color) {
 
-    this.pos = createVector(x, y);
+    this.position = createVector(x, y);
 
-    this.c = c; //color
+    this.color = color; //color
 
     this.text = text; // text do be typed
     this.size = text.length * 15; //size
@@ -18,24 +18,57 @@ function Asteroid(x, y, text, c) {
 Asteroid.prototype.update = function() {
 
     // mak speed based upon score
-    this.possition.y += map (score, 0, 1000, 1, 15);
+    this.position.y += map (score, 0, 1000, 1, 15);
 
-    if (this.possition.y > height) {
+    if (this.position.y > height) {
         endGame();
     }
 };
 
+/**
+ * based upon keyCode, will add to the completedText
+ */
+Asteroid.prototype.erode = function(keyCode){
+    var inputChar = String.fromCharCode(keyCode).toLowerCase(); // keyCode to Char
+    var length = this.completedText.length + 1;
+
+    if (this.text.substring(0, length) === this.completedText + inputChar) // id the character matches text
+        this.completedText += inputChar;
+
+    this.intact = (this.completedText !== this.text); // update intact
+};
+
+/**
+ * drawn Asteroid
+ */
 Asteroid.prototype.draw = function() {
 
-    fill(this.c);
+    fill(this.color);
 
     stroke(0);
     strokeWeight(3);
-    ellipse(this.pos.x, this.pos.y, this.s);
+    ellipse(this.position.x, this.position.y, this.size);
 
     noStroke();
+    textAlign(CENTER);
     textSize(20);
     fill(255);
-    text(this.text, this.pos.x, this.pos.y);
-
+    text(this.text, this.position.x, this.position.y);
 };
+
+/**
+ * figures out which Asteroid within the field array
+ * should be targeted
+ */
+function findAsteroid(code, field) {
+    var char = String.fromCharCode(code).toLowerCase();
+
+    for (var i = 0; i < field.length; i++) {
+        if (field[i].text.startsWith(char)){
+            
+            return field[i];
+        }
+    }
+
+    return null;
+}
